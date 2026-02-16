06a5ff4533e2fac165749acc15453b07c54a9761
iaifrns <nsinifranc@gmail.com>
2026-02-16 14:06:33 +0000

    modifications

---
+++ src/controller/studentController.js
@@ -0,0 +1,41 @@
+import { prisma } from "../lib/prisma.js";
+
+export const getUserProfile = async (req, res) => {
+    try{
+        const userId = req.params.userId
+
+        const user = await prisma.student.findUnique({
+            where : {
+                id: userId
+            },
+            select: {
+                id: true,
+                firstName: true,
+                lastName:true,
+                email: true,
+                classLevel: true,
+                phoneNumber: true
+            }
+        });
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "The student do not exists."
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data:user
+        });
+
+    } catch (error){
+        console.error("Fail to fetch student data: ", error);
+        res.status(500).json({
+            success: false,
+            message: "server error"
+        });
+
+    }
+};
