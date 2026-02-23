31fde5f272b596e5b548a0b3bdfb70ba3349b71e
FrankieLiuF <frankieliu722@gmail.com>
2026-02-23 16:07:15 +0000

    upgrading

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { prisma } from "../config/db.js";
+import { prisma } from "../lib/prisma.js";
 
 // Read the token from the request
 // Check if token is valid
--- src/routes/studentRoute.js
+++ src/routes/studentRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { getUserProfile } from "../controllers/studentController.js";
+import { getUserProfile } from "../controller/studentController.js";
 
 const router = express.Router();
 
