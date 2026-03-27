fce63bd9ee6a7dbf9ac289fd43c4657744204bbd
Jefferson-cobbinah <jeffersoncobbina6@gmail.com>
2026-03-26 20:19:47 -0500

    update the code on git

--- src/routes/createTFRoute.ts
+++ src/routes/createTFRoute.ts
@@ -1,5 +1,14 @@
 import express from 'express';
-import {createTask, createFeedback, updateTask, deleteTask} from '../controller/createTFController';
+//import {createTask, createFeedback, updateTask, deleteTask} from '../controller/createTFController';
+import {
+    createTask,
+    createFeedback,
+    getAllTasks,
+    getTaskById,
+    getFeedbackForTask,
+    updateTask,
+    deleteTask
+} from '../controller/createTFController';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -15,5 +24,14 @@ router.put('/update/:taskId', authMiddleware, updateTask);
 
 //DELETE - delete task (admin only)
 router.delete('/delete/:taskId', authMiddleware, deleteTask);
+
+// GET- get all feedback for task
+router.get('/:taskId/feedback', authMiddleware, getFeedbackForTask);
+
+// GET - all tasks
+router.get('/', authMiddleware, getAllTasks);
+
+// GET - single task by ID
+router.get('/:taskId', authMiddleware, getTaskById);
 
 export default router;
